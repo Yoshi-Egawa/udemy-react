4e94ac4667943cbe4cbe6935312288816675ffcb
Yoshi-Egawa <71691411+Yoshi-Egawa@users.noreply.github.com>
2021-11-21 23:56:08 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -10,7 +10,8 @@ export default function App() {
 
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
-
+  // const onClickClose = () => setOpen(false); // 関数が再生成されるので、メモ化が聞かない
+  const onClickClose = useCallback(() => setOpen(false), [open]); // []見張る値
   const onClickCountup = () => {
     setCount(count + 1);
   };
@@ -21,7 +22,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
