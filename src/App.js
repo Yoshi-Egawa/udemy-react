316bcfc00e0655aa3146956dd060024a64f524a0
Yoshi-Egawa <71691411+Yoshi-Egawa@users.noreply.github.com>
2021-11-22 21:44:50 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,6 +12,10 @@ export default function App() {
   const onClickOpen = () => setOpen(!open);
   // const onClickClose = () => setOpen(false); // 関数が再生成されるので、メモ化が聞かない
   const onClickClose = useCallback(() => setOpen(false), [open]); // []見張る値
+
+  const temp = useMemo(() => 1 + 3, []); // 変数のメモ化: 複雑な処理などの再実行を防ぐ
+  console.log(temp);
+
   const onClickCountup = () => {
     setCount(count + 1);
   };
